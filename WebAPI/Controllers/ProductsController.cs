73cce0512009f8b010e8427a9c15ec007ad12331
Atilla <atillaagungorr@gmail.com>
2023-11-18 20:45:37 +0300

    Web API layer added to project

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //attribute
+    public class ProductsController : ControllerBase
+    {
+        //IoC Container
+        //Naming convention
+        //Loosely coupled
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //dependency chain
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
